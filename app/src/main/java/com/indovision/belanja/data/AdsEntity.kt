c6ff1251a3a715706b2cf28c560d5f6cfa290280
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-17 04:06:46 +0700

    rename update entity file

--- app/src/main/java/com/indovision/belanja/data/AdsEntity.kt
+++ app/src/main/java/com/indovision/belanja/data/AdsEntity.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class AdsEntitiy(
+data class AdsEntity(
     val id: String = "",
     val imagePath: String = ""
 ) : Parcelable {
