5d065368c6ae4ab889f58ebe5c51d65cb41ca455
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-18 23:57:05 +0700

    add api service

---
+++ app/src/main/java/com/indovision/belanja/data/source/remote/ApiService.kt
@@ -0,0 +1,12 @@
+package com.indovision.belanja.data.source.remote
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("events")
+    fun getEvents() : Call<EventResponse>
+
+    @GET("products")
+    fun getRecommendationProducts() : Call<ProductResponse>
+}
