0af4d193600cf00a36e56f0ecf5fff5806150969
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-24 17:24:24 +0700

    finish detail product activity

--- app/src/main/java/com/indovision/belanja/ui/detail/DetailProductActivity.kt
+++ app/src/main/java/com/indovision/belanja/ui/detail/DetailProductActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.ViewModelProvider
 import com.indovision.belanja.R
+import com.indovision.belanja.data.source.remote.RemoteDataSource
 import com.indovision.belanja.databinding.ActivityDetailProductBinding
+import com.indovision.belanja.ui.detail.adapter.ProductImagePagerAdapter
+import com.indovision.belanja.viewmodel.DetailProductViewModel
+import com.indovision.belanja.viewmodel.ViewModelFactory
 
 class DetailProductActivity : AppCompatActivity() {
+    companion object{
+        const val EXTRA_ID = "extra_id"
+    }
+
     private lateinit var viewBinding: ActivityDetailProductBinding
+    private lateinit var viewModel : DetailProductViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +34,27 @@ class DetailProductActivity : AppCompatActivity() {
             )
         )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val factory = ViewModelFactory.getInstance(RemoteDataSource())
+        viewModel = ViewModelProvider(this, factory)[DetailProductViewModel::class.java]
+
+        getData()
+    }
+
+    private fun getData() {
+        viewModel.getProductDetail(intent.getStringExtra(EXTRA_ID).toString())
+            .observe(this,{
+                val viewPagerAdapter = ProductImagePagerAdapter(it.imagePath)
+                with(viewBinding){
+                    vpProductImages.adapter = viewPagerAdapter
+                    dotIndicatorImages.setViewPager2(vpProductImages)
+                    productDescriptions.text = it.description
+                    productPrices.text = it.price
+                    productTitle.text = it.name
+                    shopAddress.text = it.shopEntity.address
+                    shopName.text = it.shopEntity.name
+                }
+            })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
