00f973ad7badaddcc02616c34d1fda30db4b792f
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-24 15:36:56 +0700

    add detail product response

--- app/src/main/java/com/indovision/belanja/data/source/remote/response/DetailProductResponse.kt
+++ app/src/main/java/com/indovision/belanja/data/source/remote/response/DetailProductResponse.kt
@@ -2,14 +2,7 @@ package com.indovision.belanja.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class ProductResponse(
-    @field:SerializedName("message")
-    val message: String,
-    @field:SerializedName("data")
-    val products: List<Product>
-)
-
-data class Product (
+data class DetailProductResponse(
     @field: SerializedName("product_id")
     val id: String,
     @field: SerializedName("product_name")
@@ -19,16 +12,7 @@ data class Product (
     @field: SerializedName("price_sell")
     val price: String,
     @field: SerializedName("image_path")
-    val imagePath: String,
+    val imagePath: Array<String>,
     @field: SerializedName("shop")
     val shop: Shop
-)
-
-data class Shop (
-    @field: SerializedName("shop_id")
-    val id: String,
-    @field: SerializedName("shop_name")
-    val name: String,
-    @field: SerializedName("shop_address")
-    val address: String
 )
