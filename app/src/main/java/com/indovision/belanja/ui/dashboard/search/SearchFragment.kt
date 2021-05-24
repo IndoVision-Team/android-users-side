305c866a46ad2782adfa86cf6425f27ba17a4138
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-24 15:33:36 +0700

    finish search fragment

--- app/src/main/java/com/indovision/belanja/ui/dashboard/search/SearchFragment.kt
+++ app/src/main/java/com/indovision/belanja/ui/dashboard/search/SearchFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.indovision.belanja.data.source.remote.RemoteDataSource
 import com.indovision.belanja.databinding.FragmentSearchBinding
+import com.indovision.belanja.ui.dashboard.search.adapter.SearchAdapter
+import com.indovision.belanja.viewmodel.SearchViewModel
+import com.indovision.belanja.viewmodel.ViewModelFactory
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
@@ -17,6 +23,20 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        val factory = ViewModelFactory.getInstance(RemoteDataSource())
+        val viewModel = ViewModelProvider(this, factory)[SearchViewModel::class.java]
+
+        viewModel.getProductSearch(SearchFragmentArgs.fromBundle(arguments as Bundle).searchText)
+            .observe(viewLifecycleOwner, {
+                val adapter = SearchAdapter(it)
+                with(binding) {
+                    rvResultSearch.adapter = adapter
+                    rvResultSearch.setHasFixedSize(true)
+                    rvResultSearch.layoutManager =
+                        GridLayoutManager(context, 2)
+                }
+            })
         return binding.root
     }
 
