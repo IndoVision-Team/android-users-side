7524a2d215e386264cc7d23218c15e5dbb5a8f9f
lyongki <lyongkisuryonotaniaji@gmail.com>
2021-05-28 14:00:23 +0700

    add cart response

---
+++ app/src/main/java/com/indovision/belanja/data/source/remote/response/CartResponse.kt
@@ -0,0 +1,25 @@
+package com.indovision.belanja.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class CartResponse(
+    @field:SerializedName("message")
+    var message: String,
+    @field:SerializedName("cart_user_id")
+    var userId: String,
+    @field:SerializedName("cart_status")
+    var status: String,
+    @field:SerializedName("cart_items")
+    var cartItems: List<CartItem>
+)
+
+data class CartItem(
+    @field:SerializedName("item_id")
+    var id: String,
+    @field:SerializedName("item_name")
+    var name: String,
+    @field:SerializedName("item_price")
+    var price: String,
+    @field:SerializedName("item_quantity")
+    var quantity: String
+)
